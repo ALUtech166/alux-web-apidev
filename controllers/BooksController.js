675cb0961e10d3e458e06cb7caabf4265b9bb5aa
= <=>
2021-03-16 18:27:21 +0200

    books

--- controllers/BooksController.js
+++ controllers/BooksController.js
@@ -1,4 +1,4 @@
-import Member from "../models/books.model.js";
+import Book from "../models/books.model.js";
 
 //add new book
 
@@ -24,4 +24,127 @@ export async function addBook(req, res) {
             message: "Oopss! Something is wrong..."
         })
     }
+}
+
+
+
+
+//View a Book
+export async function viewBook(req, res) {
+    try {
+        let allBooks = await Books.findAll({where: {book_id: req.params.id}});
+        if (allBooks) {
+            res.json({
+                success: true,
+                message: 'Book records retrieved successfully',
+                data: allBooks
+            })
+        } else {
+            res.json({
+                success: true,
+                message: 'No Book records found.',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Oopss! Something is wrong..."
+        })
+    }
+}
+
+//View all Books
+export async function viewAllBooks(req, res) {
+    try {
+        let allBooks = await Books.findAll();
+        if (allBooks) {
+            res.json({
+                success: true,
+                message: 'Book records retrieved successfully',
+                data: allBooks
+            })
+        } else {
+            res.json({
+                success: true,
+                message: 'No Book records found.',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Oopss! Something is wrong..."
+        })
+    }
+}
+
+//Update Book record
+export async function updateBook(req, res) {
+    try{
+       let Book = await Books.findAll({where: {book_id: req.params.Book_id}});
+       if (Book) {
+           await Books.update(req.body, {where : {book_id: req.params.Book_id}})
+           .then(flag => {
+               if (flag == 1){
+                   res.json({
+                       success: true, 
+                       message: "Book updated succusfully"
+                   })
+               } else {
+                   res.json({
+                       success: false, 
+                       message: "Book was not updated"
+                   })
+               }
+           });
+           
+       } else {
+           res.json({
+               success: false,
+               message: 'No Book records found.',
+           })
+       }
+   } catch (err) {
+       console.log(err);
+       res.status(500).json({
+           success: false,
+           message: "Oopss! Something is wrong..."
+       })
+   }
+}
+
+//Delete a Book
+export async function deleteBook(req, res) {
+   try{
+       let Book = await Books.findAll({where: {book_id: req.params.id}});
+       if (Book) {
+           await Books.destroy({where : {book_id: req.params.id}})
+           .then(flag => {
+               if (flag == 1){
+                   res.json({
+                       success: true, 
+                       message: "Book deleted succusfully"
+                   })
+               } else {
+                   res.json({
+                       success: false, 
+                       message: "Book was not deleted"
+                   })
+               }
+           });
+           
+       } else {
+           res.json({
+               success: false,
+               message: 'No Book records found.',
+           })
+       }
+   } catch (err) {
+       console.log(err);
+       res.status(500).json({
+           success: false,
+           message: "Oopss! Something is wrong..."
+       })
+   }
 }
