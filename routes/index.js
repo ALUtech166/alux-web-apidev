45c1ed06f4f68448178495feb70bf574ead74b69
= <=>
2021-03-16 17:52:48 +0200

    remove all

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import membersRouter from "./membersRouter.js"
-
-const router = express.Router();
-
-
-router.use("/members", membersRouter);
-
-export default router;
